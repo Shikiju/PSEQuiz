9ef35a148e5c0a9d37c04e1c827c797b208e7c9c
Shikiju <koenvbaast25@gmail.com>
2022-05-05 22:22:35 +0200

    Initial Commit
    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,235 @@
+export const allData = [
+  {
+    id: 0,
+    question: "Een bouwvakker valt van de steiger en loopt daarbij een slagaderlijke <br>\
+    bloeding op aan zijn pols. Zijn collega knevelt de arm en brengt het<br>\
+    slachtoffer met zijn auto naar het ziekenhuis. Op sommige stukken<br>\
+    binnen de bebouwde kom rijdt hij 90 km per uur.<br><br>\
+    \
+    De collega reed veel te snel. Op welke strafuitsluitingsgrond kan hij<br>\
+    zich beroepen? ",
+    answer: "Overmacht in de zin van noodtoestand",
+    variants: [`Psychische overmacht`, `Noodweer`, `Overmacht in de zin van noodtoestand`, `Noodweerexces`]
+  },
+  {
+    id: 1,
+    question: "Een toezichthouder wilde namen weten van de personen die werkzaam zijn in een<br>\
+    café. Een schilder is bezig om de bar in het café te schilderen. De toezichthouder wil<br>\
+    van hem ook de personalia weten en vordert zijn identiteitsbewìjs.<br><br>\
+    \
+    Is deze toezichthouder hiertoe bevoegd?",
+    answer: "Hij is bevoegd zijn identiteitsbewijs te vorderen",
+    variants: [`Hij is niet bevoegd zijn identiteitsbewijs te vorderen`, `Hij is bevoegd zijn identiteitsbewijs te vorderen`, `Hij is bevoegd het identiteitsbewijs te vorderen, maar als de schilder er niet aan
+    voldoet is deze niet strafbaar.`]
+  },
+  {
+    id: 2,
+    question:
+      "Een gerechtsdeurwaarder wil een woning tegen de wil van de bewoner betreden om een<br>\
+      vonnis te executeren. De bewoner weigert de gerechtsdeurwaarder de toegang. De<br>\
+      deurwaarder vraagt de politie om hulp. Mag de deurwaarder de politie inschakelen om zo<br>\
+      nodig de deur van die woning te forceren?",
+    answer: "Ja, de gerechtsdeurwaarder is bevoegd om de woning te betreden en kan hiervoor de hulp van de politie inroepen",
+    variants: [`Nee, de politie moet eerst een machtiging tot binnentreden hebben van een (hulp)officier van justitie`, `Ja, de gerechtsdeurwaarder is bevoegd om de woning te betreden en kan hiervoor de hulp van de politie inroepen`, `Ja, de politie moet dan wel een machtiging tot binnentreden hebben van de burgemeester`]
+  },
+  {
+    id: 3,
+    question: "Een politieagent bemiddelt bij een conflict tussen buren en schrijft<br>\
+    de nodige gegevens op, onder andere de identiteitsgegevens van de<br>\
+    betrokkenen. Er is geen sprake van een strafbaar feit.<br>\
+    <br>\
+    Op grond van welke wet mocht de agent de gegevens noteren?",
+    answer: "De wet politiegegevens",
+    variants: [`De wet politiegegevens`, `De politiewet`, `Het wetboek van strafvordering`, `De AVG`]
+  },
+  {
+    id: 4,
+    question:
+      "Wie kunnen slachtoffer zijn volgens het Wetboek van Strafvordering?",
+    answer: "Natuurlijke personen en rechtspersonen",
+    variants: [`Natuurlijke personen en de aangewezen rechtspersonen`, `Natuurlijke personen en rechtspersonen`, `Natuurlijke personen`, `Alleen rechtspersonen`]
+  },
+  {
+    id: 5,
+    question: "Een man opent overdag de deur van een flatwoning van een ander door een<br>\
+    betaalpasje tussen de deur en de stijl te steken en de dagschoot terug te drukken<br>\
+    (flippermethode). Hij gaat naar binnen, neemt geld en sieraden weg en verlaat de<br>\
+    woning.<br>\
+    <br>\
+    Welk strafbaar feit heeft hij gepleegd?",
+    answer: "Diefstal door middel van een valse sleutel",
+    variants: [`Diefstal door middel van insluiping, 311 Sr`, `Diefstal door middel van een valse sleutel`, `Diefstal door middel van braak`, `Diefstal door middel van verbreking`]
+  },
+  {
+    id: 6,
+    question:
+      "Een op heterdaad betrapte winkeldief rent weg om aan zijn aanhouding door de<br>\
+      beveiliging te ontsnappen.<br>\
+      <br>\
+      De dief rent een kerk in waar geen gebedsdienst aan de gang is.<br>\
+      Mag de beveiliging de kerk betreden ter aanhouding?",
+    answer: "ja, want er is geen godsdienstoefening bezig",
+    variants: [`nee, want beveiligers zijn geen opsporingsambtenaren en mogen geen plaatsen
+    betreden ter aanhouding`, `ja, want er is geen godsdienstoefening bezig`, `nee, gebedshuizen zoals kerken en moskeeën, mogen niet worden betreden ter
+    aanhouding`, `ja, als de beveiliging vooraf overleg heeft gepleegd met een hulpofficier of officier van
+    justitie`]
+  },
+  {
+    id: 7,
+    question: "Wie is voorzitter van het college van Burgemeester en Wethouders?",
+    answer: "De Burgemeester",
+    variants: [`De Burgemeester`, `De Gemeenteraad`, `De Wethouder van Algemene zaken`, `Dit College kent geen voorzitter`]
+  },
+  {
+    id: 8,
+    question: "Het is nacht als een inbreker probeert binnen te komen in een woning. Op het moment<br>\
+    dat hij bezig is om met een ijzerdraadje de voordeur te openen wordt hij door een<br>\
+    politieagent betrapt en aangehouden.<br>\
+    <br>\
+    Is de inbreker strafbaar?",
+    answer: "Ja, voor poging tot gekwalificeerde diefstal",
+    variants: [`Nee, hier is geen sprake van een begin van uitvoering`, `Ja, voor poging tot gekwalificeerde diefstal`, `Ja, voor voorbereiding tot gekwalificeerde diefstal`, `Nee, want de man is nog geen verdachte`]
+  },
+  {
+    id: 9,
+    question: "Een man doet aangifte bij de politie van diefstal van zijn laptop, terwijl hij weet dat die<br>\
+    niet is gestolen.<br>\
+    <br>\
+    Welk strafbaar feit pleegt deze man?",
+    answer: "Het doen van een valse aangifte",
+    variants: [`Hij moet klachte doen, het is een klachtdelict`, `Poging tot het doen van een valse aangifte`, `Geen enkel strafbaar feit`, `Het doen van een valse aangifte`]
+  },
+  {
+    id: 10,
+    question: "Een omstander maakt foto's van een bekeuringssituatie. Een politieagent vordert daarop<br>\
+    van deze man een geldig identiteitsbewijs.<br>\
+    <br>\
+    Mag de agent het identiteitsbewijs vorderen?",
+    answer: "Nee, dit is niet noodzakelijk voor de uitvoering van zijn taak",
+    variants: [`Ja, de man mag niet ongevraagd een foto van de agent nemen`, `Nee, dit is niet noodzakelijk voor de uitvoering van zijn taak`, `Nee, hij moet het de man eerst vragen voordat hij begint te vorderen`, `Ja, de man is verplicht mee te werken`]
+  },
+  {
+    id: 11,
+    question: "Een minister wordt omgekocht. De minister wordt na onderzoek hierover gehoord door<br>\
+    de politie en de minister moet voor de rechter verschijnen.<br>\
+    <br>\
+    Voor welk gerecht moet hij in eerste instantie voorkomen?",
+    answer: "De Hoge Raad",
+    variants: [`Het gerechtshof`, `De Raad van State`, `De arrondissementsrechtbank`, `De Hoge Raad`]
+  },
+  {
+    id: 12,
+    question: "In welk antwoord staan de rangen voor de politieambtenaren van hoog naar laag<br>\
+    weergegeven?",
+    answer: "Eerste hoofdcommissaris, hoofdcommissaris, inspecteur, hoofdagent",
+    variants: [`Hoofdinspecteur, commissaris, inspecteur, aspirant`, `Hoofdcommissarìs, eerste hoofdcommissaris, eerste hoofdinspecteur,
+    hoofdinspecteur`, `Eerste hoofdcommissaris, hoofdcommissaris, inspecteur, hoofdagent`, `Hoofdagent, agent, aspirant, surveillant van politie`]
+  },
+  {
+    id: 13,
+    question: "Een buurtbewoner ziet dat een jongen met een ijzeren staaf de ruiten van een bushokje<br>\
+    kapot slaat.<br>\
+    <br>\
+    Wat mag de buurtbewoner nu doen?",
+    answer: "Hij mag de jongen aanhouden en moet hem overdragen aan een opsporingsambtenaar",
+    variants: [`Hij mag de jongen aanhouden en moet hem overdragen aan een opsporingsambtenaar`, `Hij mag de jongen staande houden en moet hem dan overdragen aan een
+    opsporingsambtenaar`, `Hij mag de jongen staande houden en zijn personalia noteren`, `Hij heeft geen bevoegdheden want de jongen is geen verdachte`]
+  },
+  {
+    id: 14,
+    question: "Een man heeft ruzie met zijn buurvrouw. Op een dag ziet hij haar fiets voor haar woning<br>\
+    staan met het sleuteltje nog in het beugelslot. Hij haalt het sleuteltje uit het slot en gooit<br>\
+    het in het water.<br>\
+    <br>\
+    Waaraan maakt deze man zich schuldig?",
+    answer: "Vernieling",
+    variants: [`Vernieling`, `Diefstal`, `Verduistering`, `Gekwalificeerde diefstal`]
+  },
+  {
+    id: 15,
+    question: "Wanneer wordt de Raad voor de Rechtsbijstand in kennis gesteld als er een moord is<br>\
+    gepleegd? (antwoord beetje onzeker?)",
+    answer: "Nadat de beslissing is genomen dat de verdachte in verzekering wordt gesteld",
+    variants: [`Nadat de verdachte is aangehouden`, `Nadat de Officier van Justitie toestemming heeft verleend tot aanhouding buiten
+    heterdaad`, `Nadat de beslissing is genomen dat de verdachte in verzekering wordt gesteld`, `Nadat de verdachte daarom heeft gevraagd`]
+  },
+  {
+    id: 16,
+    question: "Een echtpaar loopt gearmd op straat. Een groep jongen mannen valt hen lastig en de<br>\
+    man wordt hard in zijn kruis getrapt. De vrouw wordt niet aangeraakt, er wordt niets<br>\
+    tegen haar gezegd, maar zij is zo geschrokken dat zij hierdoor niet meer de straat op<br>\
+    durft.<br>\
+    <br>\
+    Is de vrouw formeel slachtoffer van dit voorval?",
+    answer: "Ja, zij heeft rechtstreeks nadeel ondervonden",
+    variants: [`Ja, zij heeft rechtstreeks nadeel ondervonden`, `Nee, zij moet rechtstreeks vermogensschade oplopen`, `Nee, zij moet rechtstreeks letsel oplopen`, `Nee, zij moet rechtstreeks nadeel ondervinden`]
+  },
+  {
+    id: 17,
+    question: "Een werknemer wordt bij een werkgever op staande voet ontslagen. Het blijkt dat de<br>\
+    werknemer aangegeven heeft dat hij homoseksueel is en dat hij daarom zonder verdere<br>\
+    reden is ontslagen. De werknemer is kwaad en teleurgesteld en doet melding van<br>\
+    discriminatie.<br>\
+    <br>\
+    Waar wordt in eerste aanleg geregeld dat er niet gediscrimineerd mag worden?",
+    answer: "De Grondwet",
+    variants: [`De antidiscriminatiewet`, `De Wet gelijke beginselen`, `Wetboek va strafvordering`, `De Grondwet`]
+  },
+  {
+    id: 18,
+    question: "Een politieagent is bezig met de afhandeling van een autokraak. Bij de aanhouding van<br>\
+    de verdachte treft hij een autoradio en een zonnebril aan die uit de auto zijn<br>\
+    weggenomen. Hij neemt deze goederen in beslag. De verdacht bekent de auto-inbraak<br>\
+    en tekent een afstandsverklaring voor beide goederen.<br>\
+    <br>\
+    Mag de politieagent de goederen teruggeven aan de aangever?",
+    answer: "Ja, nadat de hulpofficier van justitie hiertoe opdracht heeft gegever",
+    variants: [`Nee, dit mag uitsluitend na uitspraak door een rechter`, `Ja, een agent mag inbeslaggenomen goederen altijd teruggeven aan de
+    aangeven`, `Ja, nadat de hulpofficier van justitie hiertoe opdracht heeft gegever`, `Nee, dit mag pas nadat de rechter-commissaris hierover heeft beslist`]
+  },
+  {
+    id: 19,
+    question: "In een proces-verbaal moeten de redenen van wetenschap worden weergeven.<br>\
+    <br>\
+    In welk van de onderstaande voorbeelden is een Redenen van Wetenschap weer<br>\
+    gegeven? (antwoord niet helemaal zeker?)",
+    answer: "Ik zag dat de man in mijn richting keek",
+    variants: [`Ik merkte de man aan als verdachte van vernieling`, `De motor had een cilinderinhoud van 80cc`, `De man liep hard weg`, `Ik zag dat de man in mijn richting keek`]
+  },
+  {
+    id: 20,
+    question: "Een verdachte wordt staande gehouden door een opsporingsambtenaar (OA) die hem<br>\
+    zojuist een overtreding zag plegen. De OA deelt de verdachte mee van welk feit hij hem<br>\
+    verdenkt en vraagt de verdachte vervolgens naar zijn identiteitsgegevens. De verdachte<br>\
+    weigert zijn identiteitsgegevens op te geven aan de DA.<br>\
+    <br>\
+    Mag de verdachte dit weigeren?",
+    answer: "Ja, de verdachte mag straffeloos weigeren zijn identiteitsgegevens op te geven",
+    variants: [`Ja, de verdachte mag straffeloos weigeren zijn identiteitsgegevens op te geven`, `Ja, want de cautie was nog niet gegeven`, 
+    `Nee, verdachte is nu strafbaar voor het niet opgeven van zijn identiteitsgegevens`, `Nee, de verdachte is nu strafbaar voor het niet tonen van een identiteitsbewijs`]
+  },
+  {
+    id: 12,
+    question: "",
+    answer: "A",
+    variants: [`A`, `B`, `C`, `D`]
+  },
+  {
+    id: 12,
+    question: "",
+    answer: "A",
+    variants: [`A`, `B`, `C`, `D`]
+  },
+  {
+    id: 12,
+    question: "",
+    answer: "A",
+    variants: [`A`, `B`, `C`, `D`]
+  },
+  {
+    id: 12,
+    question: "",
+    answer: "A",
+    variants: [`A`, `B`, `C`, `D`]
+  },
+];
